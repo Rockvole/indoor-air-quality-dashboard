aa6d82333c8615e07bbe686a5197473339a8ff76
Rockvole <rockvole@gmail.com>
2014-09-04 16:14:37 -0700

    Rename temperature.ino to firmware.ino

--- firmware/register_core.sh
+++ firmware/register_core.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-curl "http://foodaversions.com/dht22/initialize_core.php?core_id=53ff70065067544807400687&name=rockvole3&tz=America/Los_Angeles"
+curl "http://foodaversions.com/dht22/initialize_core.php?core_id=53ff70065067544807400687&name=Live_Home&tz=America/Los_Angeles"
 
 
