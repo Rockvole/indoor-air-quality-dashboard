684ce033c3f380d2c903519f8dd4bf1f3bd8a6b2
Rockvole <rockvole@gmail.com>
2014-10-14 14:21:19 -0700

    Add dust and sewer health information.

--- www/graphs/sewer.php
+++ www/graphs/sewer.php
@@ -4,18 +4,15 @@ include 'graph_base.php';
 $result=mysqli_query($conn,"SELECT * from readings WHERE core_id=$id and ts>= $start_ts and ts<= $end_ts order by ts"); 
 $ts=Array();
 $sewer=Array();
-$level=Array();
 while($row = mysqli_fetch_array($result)) {
 	$ts_str=gmdate('r', $row['ts']);
 	error_log("temp=".$row['temperature']."||humidity=".$row['humidity']."||ts=".$row['ts']."||ts=".$ts_str);
 	$ts[]=$row['ts'];
 	$sewer[]=$row['sewer'];
-	$level[]=1000;
 }
 $sewer_plot=new LinePlot($sewer,$ts);
 $sewer_plot->SetColor('darkgoldenrod');
 $sewer_plot->SetWeight(2);
-$level_plot=new LinePlot($level,$ts);
 
 $graph = new Graph($width,$height);
 $graph->SetFrame(false);
@@ -23,7 +20,7 @@ $graph->SetBackgroundImage('traffic33_66.png',BGIMG_FILLPLOT);
 $graph->SetBackgroundImageMix(35);
 $graph->SetMargin(60,60,40,50);
 $graph->SetMarginColor('white');
-$graph->SetScale('datlin',0,1500);
+$graph->SetScale('datlin',0,100);
 $graph->Add($sewer_plot);
 
 $graph->xaxis->SetLabelAngle(90);
@@ -35,7 +32,7 @@ $graph->yaxis->SetWeight(2);
 $graph->yaxis->SetColor('darkgoldenrod');
 $graph->yaxis->SetFont(FF_ARIAL,FS_NORMAL,$font_size-3);
 $graph->yaxis->title->SetColor('darkgoldenrod');
-$graph->yaxis->title->Set('Sewer');
+$graph->yaxis->title->Set('% Sewer');
 $graph->yaxis->title->SetFont(FF_ARIAL,FS_BOLD,$font_size);
 $graph->yaxis->title->SetAngle(90);
 $graph->yaxis->title->SetMargin(10);
