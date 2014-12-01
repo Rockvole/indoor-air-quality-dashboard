3923fabec9689273aa86f321a64c8d91fcf61088
Rockvole <rockvole@gmail.com>
2014-12-01 12:03:47 -0800

    Add icons to front page so we can direct to multiple pages

--- www/index.php
+++ www/index.php
@@ -2,8 +2,21 @@
   <head>
     <title>Welcome Page</title>
     <link rel="stylesheet" type="text/css" href="html/stylesheet.css">    
+    <style>
+      th {text-align:right;}
+      td {padding: 4px 14px 4px 14px}
+    </style>
   </head>
-  
+  <script>
+    function click_button(id, action) {
+      if(action==null)
+        document.aq.action = "year_cal.php";
+      else document.aq.action = action;
+      
+      document.aq.id.value=id;
+      document.aq.submit();
+    }
+  </script>
   <body>
 <?php
 include 'globals.php';
@@ -16,17 +29,21 @@ $result=mysqli_query($conn,"SELECT * from cores");
 
 echo "<table border=0 width='100%'><tr>";
 echo "<td><h2>Choose Sensor</h2></td>";
-echo "<td><input type='button' value='Add New Sensor' onclick='location.href=\"add_new_sensor.php\"'></td></tr>";
+echo "<td><input type='button' value='Add New Sensor' onclick='click_button(null,\"add_new_sensor.php\");'></td></tr>";
 echo "</tr></table>\n";
 echo "<br/>";
 
 echo "<form action='year_cal.php' method='get' name='aq'>\n";
 echo "<input type='hidden' name='id' value='0'>\n";
 
-echo "<table border=0>\n";
+// Icons from thenounproject
+echo "<table border=0 >\n";
 while($row = mysqli_fetch_array($result)) {
   echo "<tr>\n";
-  echo "<td><input type='button' value='" . $row['name'] . "' onClick='document.aq.id.value=\"".$row['id']."\";document.aq.submit();'></td>\n";
+  echo "<th>".$row['name']."</th>\n";
+  echo "<td><img src='html/calendar.png' onclick='click_button(".$row['id'].");' height=40 width=40 style='cursor:pointer;'></td>";
+  echo "<td><img src='html/graph.png' onclick='click_button(".$row['id'].",\"dashboard.php\");' height=40 width=40 style='cursor:pointer;'></td>";
+  echo "<td><img src='html/barchart.png' onclick='click_button(".$row['id'].",\"events/event_monthly.php\");' height=40 width=40 style='cursor:pointer;'></td>";
   echo "</tr>\n";
 }
 echo "</table>\n";
