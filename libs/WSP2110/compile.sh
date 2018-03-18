5937e4caaa686a1e6a33aa81155c3ab0acea650a
Rockvole <rockvole@gmail.com>
2018-03-18 15:35:01 -0700

    Remove libs directory

--- libs/WSP2110/compile.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-echo ------------------------------------------------------------------- build
-g++ -g -c -Wall -I../SensorBase/ -std=c++11 -fPIC WSP2110.cpp -o WSP2110.so
-
-rm -f WSP2110.so
-
